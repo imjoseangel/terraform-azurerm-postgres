949382d790b2cfa878e15136d2846369875d17a3
imjoseangel <josea.munoz@gmail.com>
2022-02-02 23:01:28 +0100

    add lint

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "random_password" "password" {
 #---------------------------------------------------------
 
 resource "azurerm_postgresql_server" "main" {
-  #ts:skip=AC_AZURE_0389 RSG lock should be skipped for now.
+  #ts:skip=accurics.azure.BDR.163 PGSQL should be skipped for now.
   name                             = var.name
   location                         = local.location
   resource_group_name              = local.resource_group_name
